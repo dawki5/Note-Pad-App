b6b8de1f4d12ed2630cf878244b922f95246a00b
Cody Dawkins <dawkinscody4@gmail.com>
2022-12-19 18:55:35 -0500

    Created notes api route

---
+++ GetRoutes/Getnotes.js
@@ -0,0 +1,25 @@
+//Required file imports
+const express = require("express");
+const notesRouter = express.Router();
+
+//Pulls router info
+const writeTo = require("../db/....");
+
+// Get request for notes api
+notesRouter.get("/api/notes", function (req, res) {
+  
+  writeTo
+    .readAll(req.body)
+    .then((notes) => res.json(notes))
+    .catch((err) => console.log(err));
+});
+
+// Post request for notes api
+notesRouter.post("/api/notes", function (req, res) {
+  writeTo
+    .addNew(req.body)
+    .then((notes) => res.json(notes))
+    .catch((err) => console.log(err));
+});
+
+module.exports = notesRouter;
